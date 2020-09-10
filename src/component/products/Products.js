818b49f5ac129cf2397f7d264880a4f056c410fd
Vishvanath Surwshe <vsurwshe@gmail.com>
2020-09-10 15:17:33 +0530

    0.0.11: Showing Product list in compoent

--- src/component/products/Products.js
+++ src/component/products/Products.js
@@ -21,16 +21,36 @@ class Products extends Component {
     
     render() { 
         const { loadProductList }=this.state
-        console.log("LO", loadProductList)
         return loadProductList ? this.loading(): this.loadProductList();
     }
 
     loading=()=> <Loading />
 
     loadProductList=()=>{
-        return <h1>Products</h1>
+        const { productList }=this.props.CommonState
+        return <div className="gallery pt-5">
+		    <div className="container pt-xl-5 pt-lg-3">
+			    <h3 className="title-w3 mb-sm-5 mb-4 text-dark text-center font-weight-bold">Products </h3>
+			    <p className="title-para text-center mx-auto mb-sm-5 mb-4">This our Company Product List, here gives a quick review of our company products which is sucessfully done.</p>
+			    <ul className="demo"> 
+                    {(productList && productList.length>0)&& productList.map((item,key)=>this.loadProduct(item,key))}
+                </ul>
+            </div>
+        </div>
     }
     
+    loadProduct=(item,key)=>{
+        let image='data:image/jpeg;base64,'+item.data;
+        return  <li key={key} >
+            <div className="gallery-grid1">
+                <img src={image} alt=" " className="img-fluid" />
+                <div className="p-mask">
+                    <h4>{item.client_name}</h4>
+                    <p>{item.compnay_name}</p>
+                </div>
+            </div>
+    </li>
+    }
 }
 
 const mapStateToProps = state => { return state; };
